7dca9746c4ea9d59460cd07c6f731e80a45a9279
Yan Bortsov <dimon321853@mail.ru>
2023-07-09 17:44:36 +0300

    add pipes

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	UseGuards,
+	ParseIntPipe,
+} from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryDto } from './dto/inventory.dto';
 import { UpdateQuantityDto } from './dto/update-quantity.dto';
@@ -26,27 +36,30 @@ export class InventoryController {
 
 	@UseGuards(JwtAuthGuard)
 	@Get(':id')
-	async findOne(@Param('id') id: string) {
-		return this.inventoryService.findOne(+id);
+	async findOne(@Param('id', ParseIntPipe) id: number) {
+		return this.inventoryService.findOne(id);
 	}
 
 	@HasRoles(Role.ADMIN)
 	@UseGuards(JwtAuthGuard, RolesGuard)
 	@Patch(':id')
-	async update(@Param('id') id: string, @Body() updateInventoryDto: InventoryDto) {
-		return this.inventoryService.update(+id, updateInventoryDto);
+	async update(@Param('id', ParseIntPipe) id: number, @Body() updateInventoryDto: InventoryDto) {
+		return this.inventoryService.update(id, updateInventoryDto);
 	}
 
 	@UseGuards(JwtAuthGuard)
 	@Patch('quantity/:id')
-	async updateQuantity(@Param('id') id: string, @Body() updateQuantityDto: UpdateQuantityDto) {
-		return this.inventoryService.updateQuantity(+id, updateQuantityDto);
+	async updateQuantity(
+		@Param('id', ParseIntPipe) id: number,
+		@Body() updateQuantityDto: UpdateQuantityDto,
+	) {
+		return this.inventoryService.updateQuantity(id, updateQuantityDto);
 	}
 
 	@HasRoles(Role.ADMIN)
 	@UseGuards(JwtAuthGuard, RolesGuard)
 	@Delete(':id')
-	async remove(@Param('id') id: string) {
+	async remove(@Param('id', ParseIntPipe) id: number) {
 		return this.inventoryService.remove(+id);
 	}
 }
