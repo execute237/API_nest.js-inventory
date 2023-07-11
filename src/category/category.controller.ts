0eaa323de73dd78dfd83a2c469dd1a2f49bdd089
Yan Bortsov <dimon321853@mail.ru>
2023-07-11 19:13:26 +0300

    category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('category')
 export class CategoryController {
 	constructor(private readonly categoryService: CategoryService) {}
 
-	@Get()
-	async findAll() {}
+	@UseGuards(JwtAuthGuard)
+	@Get('all')
+	async findAll() {
+		return this.categoryService.findAll();
+	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
-	async findOne(@Param('id', ParseIntPipe) id: number) {}
+	async findOne(@Param('id', ParseIntPipe) id: number) {
+		return this.categoryService.findOne(id);
+	}
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async findAll() {
+		return this.prisma.category.findMany();
+	}
+
+	async findOne(id: number) {
+		return this.prisma.category.findUnique({ where: { id } });
+	}
+}
