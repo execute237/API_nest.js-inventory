58d048b2d55fca70ca4af6c0440e56b3eae6c847
Yan Bortsov <dimon321853@mail.ru>
2023-07-11 17:54:27 +0300

    vendor guards

--- src/vendor/vendor.controller.ts
+++ src/vendor/vendor.controller.ts
@@ -1,31 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	ParseIntPipe,
+	UseGuards,
+} from '@nestjs/common';
 import { VendorService } from './vendor.service';
 import { VendorDto } from './dto/vendor.dto';
+import { HasRoles } from '../common/decorators/has-roles.decorator';
+import { Role } from '@prisma/client';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('vendor')
 export class VendorController {
 	constructor(private readonly vendorService: VendorService) {}
 
+	@HasRoles(Role.ADMIN)
+	@UseGuards(JwtAuthGuard, RolesGuard)
 	@Post('create')
 	async create(@Body() vendorDto: VendorDto) {
 		return this.vendorService.create(vendorDto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('all')
 	async findAll() {
 		return this.vendorService.findAll();
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
 	async findOne(@Param('id', ParseIntPipe) id: number) {
 		return this.vendorService.findOne(id);
 	}
 
+	@HasRoles(Role.ADMIN)
+	@UseGuards(JwtAuthGuard, RolesGuard)
 	@Patch(':id')
 	async update(@Param('id', ParseIntPipe) id: number, @Body() vendorDto: VendorDto) {
 		return this.vendorService.update(id, vendorDto);
 	}
 
+	@HasRoles(Role.ADMIN)
+	@UseGuards(JwtAuthGuard, RolesGuard)
 	@Delete(':id')
 	async remove(@Param('id', ParseIntPipe) id: number) {
 		return this.vendorService.remove(id);
